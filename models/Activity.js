903938f9167a642eb0d4a671f1f9289cc408f1a5
Philip Karz <phillipkarz@gmail.com>
2017-10-16 15:08:48 -0700

    models

--- models/Activity.js
+++ models/Activity.js
@@ -1,10 +1,10 @@
 const 
 mongoose = require('mongoose')
 activitySchema = new mongoose.Schema({
-    address: String,
+    address: {type: String, required: true},
     rating: Number,
     trip: {type: mongoose.Schema.Types.ObjectId, ref: 'Trip'},
     user: {type: mongoose.Schema.Types.ObjectId, ref: 'User'},
-})
+}, {timestamps: true})
 
 module.exports = mongoose.model('Activity', activitySchema)
