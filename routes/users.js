5e4178d57621b4cdc15dd45bc44c26db808e8f18
Jason Rouse <jrrouse88@gmail.com>
2017-10-16 09:57:40 -0700

    routes folder with user routes added

---
+++ routes/users.js
@@ -0,0 +1,32 @@
+const
+    express = require('express')
+    passport = require('passport')
+    usersRouter = new express.Router()
+
+usersRouter.route('/signup')
+    .get((req, res) => {
+
+    })
+    .post((req, res) => {
+
+    })
+
+usersRouter.route('/login')
+    .get((req, res) => {
+
+    })
+    .post((req, res) => {
+
+    })
+
+usersRouter.route('/profile')
+    .get((req, res) => {
+
+    })
+
+usersRouter.route('/logout')
+    .get((req, res) => {
+
+    })
+
+module.exports = usersRouter
