7cba0a6db1d0cf3f7d0a23257f581241cfbc9bb7
Philip Karz <phillipkarz@gmail.com>
2017-10-16 16:20:17 -0700

    trip views

--- models/Trip.js
+++ models/Trip.js
@@ -4,6 +4,7 @@ tripSchema = new mongoose.Schema({
     name: {type: String, required: true},
     length: {type: Number, required: true},
     locale: {type: String, required: true},
+    rating: {type: Number, required: true},
     photo: String,
     user: {type: mongoose.Schema.Types.ObjectId, ref: 'User'},
  }, {timestamps: true})
