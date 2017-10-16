29daa557202d9b0adab841906ceb2aed399fd6a4
Philip Karz <phillipkarz@gmail.com>
2017-10-16 11:29:48 -0700

    users patch and delete

--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,11 @@ const
     express = require('express'),
     passport = require('passport'),
     usersRouter = new express.Router()
+    User = require('..models/User.js')
+
+
+
+
 
 usersRouter.route('/signup')
     .get((req, res) => {
@@ -29,4 +34,28 @@ usersRouter.route('/logout')
 
     })
 
+
+usersRouter.route('/:id')
+    .patch((req, res) => {
+        User.findById(req.params.id, (err, user) => {
+            if(err) return console.log(err)
+            user = Object.assign(user, req.body)
+            user.save((err, updatedUser) => {
+                if(err) return console.log(err)
+                res.json({
+                    success: true,
+                    message: 'User Updated',
+                    user: updatedUser 
+             })
+        })
+
+    })
+
+    .delete((req, res) => {
+        User.findByIdAndRemove(req.params.id, (err, user) => {
+            if(err) return console.log(err)
+            res.json(user)
+            })
+        })
+})
 module.exports = usersRouter
