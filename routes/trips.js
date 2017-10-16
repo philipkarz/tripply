8df8a0d82fcd4fff9f349f52c3b0a23a89b1bb3a
Katie Goines <katiegoines3@gmail.com>
2017-10-16 11:06:50 -0700

    trips router controller actions

--- routes/trips.js
+++ routes/trips.js
@@ -10,12 +10,32 @@ tripsRouter.route('/')
             res.json(trips)
         })
     })
+
     .post((req, res) => {
         Trip.create(req.body, (err, trip) => {
             res.json({success: true, message:"trip created", trip:trip})
         })
     })
 
+
+tripsRouter.route('/:id')
+    .get((req, res) => {
+        Trip.findById(req.params.id, (err, trip) => {
+            res.json(trip)
+        })
+    })
+
+    .patch((req, res) => {
+        Trip.findByIdAndUpdate(req.params.id, req.body, {new:true}, (err, updatedTrip) => {
+            res.json(updatedTrip)
+        })
+    })
+
+    .delete((req, res) => {
+        Trip.findByIdAndRemove(req.params.id, (err, deletedTrip) => {
+            res.json({success: true, message: `${deletedTrip.name} has been deleted.`})
+        })
+    })
 
 
 module.exports = tripsRouter
