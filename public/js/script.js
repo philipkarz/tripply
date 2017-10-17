d9d4b10db3dda1b54fa3064740241e9d35fe9da8
Philip Karz <phillipkarz@gmail.com>
2017-10-16 18:30:10 -0700

    edit

---
+++ public/js/script.js
@@ -0,0 +1,11 @@
+// console.log('script loaded')
+// var $deleteBtn = $('#delete-btn')
+// $deleteBtn.on('click', function() {
+   
+//     $.ajax({
+//         url: '/profile', method: 'DELETE',
+//       })
+//       .done((res) => { 
+//     //
+//       })
+// })
