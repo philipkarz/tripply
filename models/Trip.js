600409f25a1b4eddc57a84acd8e989d5f103859b
Cole Herzer <cole.herzer@gmail.com>
2017-10-17 10:29:53 -0700

    set up basic layout for profile ejs, added description to trip model

--- models/Trip.js
+++ models/Trip.js
@@ -4,6 +4,7 @@ tripSchema = new mongoose.Schema({
     name: {type: String, required: true},
     length: {type: Number, required: true},
     locale: {type: String, required: true},
+    description: {type: String, default: 'Description not yet added'},
     photo: String,
     user: {type: mongoose.Schema.Types.ObjectId, ref: 'User'},
  }, {timestamps: true})
