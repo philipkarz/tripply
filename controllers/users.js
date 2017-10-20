2ef34a22066dfdffd922b9ab31f0fc6cf2fa239f
Katie Goines <katiegoines3@gmail.com>
2017-10-19 18:56:04 -0700

    separated concerns for users

--- controllers/users.js
+++ controllers/users.js
@@ -80,7 +80,3 @@ const
 
     }
 
-    function isLoggedIn(req, res, next) {
-        if(req.isAuthenticated()) return next()
-        res.redirect('/')
-    }
--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,5 @@
 const
     express = require('express'),
-    passport = require('passport'),
     usersRouter = new express.Router(),
     User = require('../models/User.js'),
     Trip = require('../models/Trip.js'),
@@ -12,15 +11,12 @@ usersRouter.get('/signup', usersController.signupShow)
 usersRouter.post('/signup', usersController.signupCreate)
 usersRouter.get('/login', usersController.loginShow)
 usersRouter.post('/login', usersController.loginCreate)
-usersRouter.get('/profile', usersController.show)
-usersRouter.patch('/profile', usersController.update)
-usersRouter.post('/profile', usersController.create)
-usersRouter.delete('/profile', usersController.delete)
-usersRouter.get('/profile/edit', usersController.edit)
+usersRouter.get('/profile', isLoggedIn, usersController.show)
+usersRouter.patch('/profile', isLoggedIn, usersController.update)
+usersRouter.post('/profile', isLoggedIn, usersController.create)
+usersRouter.delete('/profile', isLoggedIn, usersController.delete)
+usersRouter.get('/profile/edit', isLoggedIn, usersController.edit)
 usersRouter.get('/logout', usersController.logout)
-
-
-
 
 function isLoggedIn(req, res, next) {
     if(req.isAuthenticated()) return next()
